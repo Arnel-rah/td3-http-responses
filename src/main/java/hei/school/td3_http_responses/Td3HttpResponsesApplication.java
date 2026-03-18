d020cd6daeb535268675891e9a1d501466546da0
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-18 17:32:32 +0300

    chore: initial project setup

---
+++ src/main/java/hei/school/td3_http_responses/Td3HttpResponsesApplication.java
@@ -0,0 +1,13 @@
+package hei.school.td3_http_responses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td3HttpResponsesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td3HttpResponsesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/td3_http_responses/Td3HttpResponsesApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.td3_http_responses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td3HttpResponsesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
